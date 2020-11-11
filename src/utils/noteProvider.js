c02e5824107b3ec58ade1d9ae2e6e8904644efc6
Bertrand Thomas <thomas.bertrand@elca.ch>
2020-11-11 21:16:03 +0100

    scale down to 1 octave

--- src/utils/noteProvider.js
+++ src/utils/noteProvider.js
@@ -1,12 +1,12 @@
 const Notes = [
-  { value: "a", nbAccents: 0, isTop: true, pitch: 8 + 12 * 4 },
-  { value: "a", nbAccents: 0, isTop: false, pitch: 6 + 12 * 4 },
-  { value: "a", nbAccents: 1, isTop: false, pitch: 4 + 12 * 4 },
-  { value: "a", nbAccents: 2, isTop: false, pitch: 3 + 12 * 4 },
-  { value: "a", nbAccents: 3, isTop: false, pitch: 1 + 12 * 4 },
-  { value: "4", nbAccents: 0, isTop: false, pitch: 11 + 12 * 3 },
-  { value: "5", nbAccents: 0, isTop: false, pitch: 10 + 12 * 3 },
-  { value: "6", nbAccents: 0, isTop: false, pitch: 8 + 12 * 3 },
+  { value: "a", nbAccents: 0, isTop: true, pitch: 8 + 12 * 3 },
+  { value: "a", nbAccents: 0, isTop: false, pitch: 6 + 12 * 3 },
+  { value: "a", nbAccents: 1, isTop: false, pitch: 4 + 12 * 3 },
+  { value: "a", nbAccents: 2, isTop: false, pitch: 3 + 12 * 3 },
+  { value: "a", nbAccents: 3, isTop: false, pitch: 1 + 12 * 3 },
+  { value: "4", nbAccents: 0, isTop: false, pitch: 11 + 12 * 2 },
+  { value: "5", nbAccents: 0, isTop: false, pitch: 10 + 12 * 2 },
+  { value: "6", nbAccents: 0, isTop: false, pitch: 8 + 12 * 2 },
 ];
 
 export const GetNote = (previous) => {
